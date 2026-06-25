6e84899d315d5183107dbc85901e2cfff83a7d8c
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-25 14:41:39 -0300

    chore: dockerize application

--- src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/Application.java
+++ src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioTecnicoEngemanBeApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesafioTecnicoEngemanBeApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
