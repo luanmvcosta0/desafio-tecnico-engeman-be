94a1f90c6c46353ee3667af35beb7a887c204fd0
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-25 12:07:08 -0300

    first commit

---
+++ src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/DesafioTecnicoEngemanBeApplication.java
@@ -0,0 +1,13 @@
+package com.luanmvcosta0.desafio_tecnico_engeman_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoEngemanBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoEngemanBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luanmvcosta0/desafio_tecnico_engeman_be/DesafioTecnicoEngemanBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanmvcosta0.desafio_tecnico_engeman_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoEngemanBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
