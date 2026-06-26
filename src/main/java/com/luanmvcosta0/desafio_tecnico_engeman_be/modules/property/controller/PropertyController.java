7cb6bb1d37d2919b7fdf437a1d54019994fe7b76
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-26 12:52:55 -0300

    feat: create PropertyController

---
+++ src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/property/controller/PropertyController.java
@@ -0,0 +1,11 @@
+package com.luanmvcosta0.desafio_tecnico_engeman_be.modules.property.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/property")
+public class PropertyController {
+
+
+}
