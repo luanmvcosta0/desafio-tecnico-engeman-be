5612c5bdfcd7d0af4a2aa5e69620d77a7087c433
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-26 12:53:51 -0300

    feat: PropertyService constructor

--- src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/property/controller/PropertyController.java
+++ src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/property/controller/PropertyController.java
@@ -1,11 +1,15 @@
 package com.luanmvcosta0.desafio_tecnico_engeman_be.modules.property.controller;
 
+import com.luanmvcosta0.desafio_tecnico_engeman_be.modules.property.service.PropertyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/property")
+@RequiredArgsConstructor
 public class PropertyController {
 
+    private final PropertyService propertyService;
 
 }
