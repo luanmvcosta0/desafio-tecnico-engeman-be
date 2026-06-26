fcdbabef6b4fd1c31ef67b82a71244f83aecc061
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-26 12:48:59 -0300

    feat: add constructor

--- src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/property/service/PropertyService.java
+++ src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/property/service/PropertyService.java
@@ -1,8 +1,13 @@
 package com.luanmvcosta0.desafio_tecnico_engeman_be.modules.property.service;
 
+import com.luanmvcosta0.desafio_tecnico_engeman_be.modules.property.repository.PropertyRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PropertyService {
+
+    private final PropertyRepository propertyRepository;
 
 }
