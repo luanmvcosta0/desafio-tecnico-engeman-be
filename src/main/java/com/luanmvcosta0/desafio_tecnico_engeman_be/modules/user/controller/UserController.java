0fd8deedf91dd3097b770c6fdb1bfe33b954050c
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-26 22:54:34 -0300

    feat: add constructors

--- src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/user/controller/UserController.java
+++ src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/user/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.luanmvcosta0.desafio_tecnico_engeman_be.modules.user.controller;
 
+import com.luanmvcosta0.desafio_tecnico_engeman_be.modules.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
 }
