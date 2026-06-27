0689d5de24419fd77ef681f36716fbdbd578a11e
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-26 22:47:40 -0300

    feat: create UserService and UserController

---
+++ src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/user/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.luanmvcosta0.desafio_tecnico_engeman_be.modules.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+}
---
+++ src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/user/service/UserService.java
@@ -0,0 +1,7 @@
+package com.luanmvcosta0.desafio_tecnico_engeman_be.modules.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
