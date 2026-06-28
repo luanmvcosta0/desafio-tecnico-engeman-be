a4b8ecdf1390964b952a4ba576d162f4e84f66a2
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-28 00:31:53 -0300

    feat: implementing UserDetails in UserEntity

--- src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/user/model/UserEntity.java
+++ src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/user/model/UserEntity.java
@@ -3,13 +3,18 @@ package com.luanmvcosta0.desafio_tecnico_engeman_be.modules.user.model;
 import com.luanmvcosta0.desafio_tecnico_engeman_be.modules.user.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity(name = "USER_TABLE")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -24,7 +29,32 @@ public class UserEntity {
     @Column(name = "password")
     private String password;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "role")
     private UserRole role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
