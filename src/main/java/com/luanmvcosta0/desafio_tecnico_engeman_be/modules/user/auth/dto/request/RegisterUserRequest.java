74292a1cbbf59d6ddc5b34e3e13c97b3752ad8df
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-27 23:50:49 -0300

    feat: add records for request and responses to login and register

---
+++ src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/user/auth/dto/request/RegisterUserRequest.java
@@ -0,0 +1,8 @@
+package com.luanmvcosta0.desafio_tecnico_engeman_be.modules.user.auth.dto.request;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public record RegisterUserRequest(@NotEmpty(message = "O username é obrigatório") String username,
+                                  @NotEmpty(message = "O email é obrigatório") String email,
+                                  @NotEmpty(message = "A senha é obrigatória") String password) {
+}
