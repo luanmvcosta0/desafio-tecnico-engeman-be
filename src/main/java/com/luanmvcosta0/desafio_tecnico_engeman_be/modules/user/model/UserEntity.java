092c7e029e6ce2e5f653aca6d06b672cfe58bf5b
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-28 13:44:49 -0300

    refactor: removing unnecessary code

--- src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/user/model/UserEntity.java
+++ src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/user/model/UserEntity.java
@@ -44,23 +44,5 @@ public class UserEntity implements UserDetails {
         return email;
     }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
-    }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
-    }
 }
