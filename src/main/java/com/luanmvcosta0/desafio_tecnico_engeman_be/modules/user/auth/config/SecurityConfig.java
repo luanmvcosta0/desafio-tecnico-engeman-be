9a55553939c14650c389f4e588d42d7324b68ba2
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-30 18:09:32 -0300

    chore: configure CORS for integration to frontend

--- src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/user/auth/config/SecurityConfig.java
+++ src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/modules/user/auth/config/SecurityConfig.java
@@ -14,16 +14,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
