264799f2e3dc130bec66ddca30419678af67ca9e
luanmvcosta0 <luanmvcosta@gmail.com>
2026-06-30 18:11:00 -0300

    chore: add a missing file to cors config

---
+++ src/main/java/com/luanmvcosta0/desafio_tecnico_engeman_be/config/cors/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.luanmvcosta0.desafio_tecnico_engeman_be.config.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+}
